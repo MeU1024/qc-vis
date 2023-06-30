e0c1a6de1eb99f0511b4b2723c114000046238a9
Wen Zhen <wlzeign@hotmail.com>
2023-06-30 18:18:41 +0800

    Update Simon_mod.py

--- benchmarks/Simon_mod.py
+++ benchmarks/Simon_mod.py
@@ -1,48 +1,36 @@
 from qiskit import QuantumCircuit
 import random
-# random.seed(555)
+import numpy as np
 
-
-def simon_oracle(b, bug=0):
+def simon_oracle(qc, b):
     """returns a Simon oracle for bitstring b"""
-    b = b[::-1]  # reverse b for easy iteration
+    b = b[::-1] # reverse b for easy iteration
     n = len(b)
-    qc = QuantumCircuit(n*2)
     # Do copy; |x>|0> -> |x>|x>
     for _index, q in enumerate(range(n)):
         qc.cx(q, q+n)
 
-    if '1' not in b:
-        return qc  # 1:1 mapping, so just exit
-
-    i = b.find('1')  # index of first non-zero bit in b
+    i = b.find('1') # index of first non-zero bit in b
     # Do |x> -> |s.x> on condition that q_i is 1
-    for q in range(n):
-        if b[q] == '1':
-            qc.cx(i, (q)+n)
-    return qc
+    indexes = np.where(np.array(list(b)) == '1')[0].tolist()
+    for q in indexes:
+        qc.cx(i, q + n)
 
 
-def get_cir(bitstring: str, bug=0) -> QuantumCircuit:
-
+def get_cir(bitstring: str) -> QuantumCircuit:
     n = len(bitstring)
     simon_circuit = QuantumCircuit(n * 2)
 
     # Apply Hadamard gates before querying the oracle
-    simon_circuit.h(range(n))
+    for q in range(n):
+        simon_circuit.h(q)
 
-    # Apply barrier for visual separation
-    simon_circuit.barrier()
-
-    simon_circuit += simon_oracle(bitstring, bug)
-
-    # Apply barrier for visual separation
-    simon_circuit.barrier()
+    simon_oracle(simon_circuit, bitstring)
 
     # Apply Hadamard gates to the input register
-    simon_circuit.h(range(n))
+    for q in range(n):
+        simon_circuit.h(q)
 
     return simon_circuit
-
 
 qc = get_cir('101')
