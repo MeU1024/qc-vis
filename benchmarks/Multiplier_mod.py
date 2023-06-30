cd6eed73b156d2e39d1ba27cc0eb8436a5a9f3d0
Wen Zhen <wlzeign@hotmail.com>
2023-06-30 17:18:03 +0800

    fix: change if to np.where

--- benchmarks/Multiplier_mod.py
+++ benchmarks/Multiplier_mod.py
@@ -1,8 +1,7 @@
-from audioop import add
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
-import sys
 import random
 import math
+import numpy as np
 
 
 def carry(qc, c0, a, b, c1):
@@ -18,7 +17,6 @@ def uncarry(qc, c0, a, b, c1):
 
 
 def carry_sum(qc, c0, a, b):
-
     qc.cx(a, b)
     qc.cx(c0, b)
 
@@ -30,7 +28,7 @@ def adder(qc, qubits):
     b = qubits[2::3]
     for i in range(0, n - 1):
         carry(qc, c[i], a[i], b[i], c[i + 1])
-    carry_sum(qc, c[n - 1], a[n - 1], b[n - 1], )
+    carry_sum(qc, c[n - 1], a[n - 1], b[n - 1])
     for i in range(n - 2, -1, -1):
         uncarry(qc, c[i], a[i], b[i], c[i + 1])
         carry_sum(qc, c[i], a[i], b[i])
@@ -45,22 +43,20 @@ def multiplier(qc, qubits):
     for i, x_i in enumerate(x):
         for a_qubit, y_qubit in zip(a[i:], y[:n - i]):
             qc.ccx(x_i, y_qubit, a_qubit)
-
         adder(qc, qubits[:3 * n])
-
         for a_qubit, y_qubit in zip(a[i:], y[:n - i]):
             qc.ccx(x_i, y_qubit, a_qubit)
 
 
-def init_bits(qc, x_bin, qubits):
-    for x, qubit in zip(x_bin, list(qubits)[::-1]):
-        if x == '1':
-            qc.x(qubit)
+def init_bits(qc, x_bin, *qubits):
+    indexes = np.where(np.array(list(x_bin)) == '1')[0]
+    r_qubits = list(qubits)[::-1]
+    for i in indexes:
+        qc.x(r_qubits[i])
 
 
 def get_cir(n):
     n_qubits = 5 * n
-    # random.seed(555)
     qr = QuantumRegister(n_qubits)
     qc = QuantumCircuit(qr)
 
@@ -71,16 +67,13 @@ def get_cir(n):
     y_bin = '{:08b}'.format(p)[-n:]
     x_bin = '{:08b}'.format(q)[-n:]
 
-    # c = qr[0:n*3:3]
-    # a = qr[1:n*3:3]
     b = qr[2:n * 3:3]
     y = qr[n * 3:n * 4]
     x = qr[n * 4:]
 
-    init_bits(qc, x_bin, x)
-    init_bits(qc, y_bin, y)
+    init_bits(qc, x_bin, *x)
+    init_bits(qc, y_bin, *y)
     multiplier(qc, qr)
     return qc
-
 
 qc = get_cir(4)
